680d2f2bb0b020dcb4f0bb364fb65fd98520efb2
HayashiKana <hpns.3537fs.kana@gmail.com>
2024-03-21 23:00:24 +0900

    change variable

--- Program.cs
+++ Program.cs
@@ -11,21 +11,21 @@ namespace Exercise601
         static void Main(string[] args)
         {
             Vector2D v1 = new Vector2D();
-            v1.x = 1.0;
-            v1.y = 1.0;
-            Console.WriteLine("v1=({0}, {1})", v1.x, v1.y );
+            v1.X = 1.0;
+            v1.Y = 1.0;
+            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y );
 
             Vector2D v2 = new Vector2D();
-            v2.x = 1.0;
-            v2.y = -1.0;
-            Console.WriteLine("v2=({0}, {1})", v2.x, v2.y );
+            v2.X = 1.0;
+            v2.Y = -1.0;
+            Console.WriteLine("v2=({0}, {1})", v2.X, v2.Y );
 
             v1.Add(v2);
-            Console.WriteLine("v1=({0}, {1})", v1.x, v1.y);
+            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
             v1.Sub(v2);
-            Console.WriteLine("v1=({0}, {1})", v1.x, v1.y);
+            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
             v1.Mul(2.0);
-            Console.WriteLine("v1=({0}, {1})", v1.x, v1.y);
+            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
             Console.WriteLine("v1,v2の内積: {0}", v1.DotProduct(v2));
         }
     }
