92f685e7b9d7f0eaf9b98025c9aa7c71fc9cba10
HayashiKana <hpns.3537fs.kana@gmail.com>
2024-03-24 22:15:15 +0900

    add Max function

---
+++ Numeric.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise601
+{
+    class Numeric
+    {
+        public static int Max(int a, int b)
+        {
+            if(a >b)
+            {
+                return a;
+            }
+            return b;
+        }
+    }
+}
