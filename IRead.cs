414fac62bb9b0881abdd15ad5c2d22a544eb2fb5
HayashiKana <hpns.3537fs.kana@gmail.com>
2024-04-02 23:37:10 +0900

    first commit

---
+++ IRead.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise601
+{
+    interface IRead
+    {
+        int Read();
+    }
+}
