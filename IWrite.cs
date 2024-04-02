93f547e870499cc7fdd96da8d0a2dc4eb486cf9b
HayashiKana <hpns.3537fs.kana@gmail.com>
2024-04-02 23:36:59 +0900

    first commit

---
+++ IWrite.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise601
+{
+    interface IWrite
+    {
+        void Write(int num);
+    }
+}
